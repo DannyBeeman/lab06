aebfcfd3ee8ec1e52fd483d5a4710d54b6997b37
mguillermo808 <mguillermo46@gmail.com>
2023-03-07 09:55:04 -0500

    added decoder function

--- main.py
+++ main.py
@@ -8,3 +8,29 @@ password = str(input())
 encoded_password = password_encoder(password)
 print(encoded_password)
 #comment
+
+def password_decoder(password): # Meynard Guillermo
+    temp_password = int(password)
+    for i in password:
+        temp_password[i] -= 3
+    return temp_password
+
+def main():
+    option = True
+    while option:
+        print("Menu")
+        print("-------------")
+        print("1. Encode\n2. Decode\n3. Quit")
+
+        option = input("Please enter an option: ")
+
+        if option == "1":
+            password_encoder(password)
+        elif option == "2":
+            password_decoder(password)
+        elif option == "3":
+            option = False
+
+
+if __name__ == "__main__":
+    main()
